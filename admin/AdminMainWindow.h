73b0e09eec110a0b9d53d11449ac933931d3ab4d
Juvin <hdcqW@outlook.com>
2025-12-19 22:07:13 +0800

    准备进行管理员后台界面开发

---
+++ admin/AdminMainWindow.h
@@ -0,0 +1,95 @@
+#pragma once
+
+#include <QMainWindow>
+#include <QTimer>
+#include <QMap>
+#include <QVector>
+#include <memory>
+
+#include "Control/DatabaseManager.h"
+
+class QStackedWidget;
+class QWidget;
+class QPushButton;
+class QLabel;
+class QLineEdit;
+class QComboBox;
+class QTableWidget;
+class QTextBrowser;
+
+class AdminMainWindow : public QMainWindow {
+    Q_OBJECT
+public:
+    explicit AdminMainWindow(QWidget *parent = nullptr);
+    ~AdminMainWindow() override = default;
+
+private slots:
+    void onRefreshTimer();  // 定时刷新同步
+
+private:
+    enum class Page {
+        Login,
+        Dashboard,
+        GearManage,
+        UserManage,
+        OrderManage,
+        SystemSettings
+    };
+
+    void setupUi();
+    QWidget* createLoginPage();
+    QWidget* createDashboardPage();
+    QWidget* createGearManagePage();
+    QWidget* createUserManagePage();
+    QWidget* createOrderManagePage();
+    QWidget* createSystemSettingsPage();
+    void switchPage(Page page);
+    
+    // 地图相关
+    void loadMapStations(QWidget *mapContainer);
+    void onStationClicked(int stationId);
+    
+    // 数据刷新
+    void refreshDashboardData();
+    void refreshGearManageData();
+    void refreshUserManageData();
+    void refreshOrderManageData();
+    
+    // 管理员登录
+    bool performAdminLogin(const QString &userId, const QString &password);
+    
+    // 天气信息（模拟）
+    QString getWeatherInfo() const;
+
+    QStackedWidget *m_stack { nullptr };
+    QString m_currentAdminId;
+    QString m_currentAdminName;
+    
+    // 导航栏
+    QPushButton *m_navGearManage { nullptr };
+    QPushButton *m_navUserManage { nullptr };
+    QPushButton *m_navOrderManage { nullptr };
+    QPushButton *m_navSystemSettings { nullptr };
+    
+    // 定时刷新（实现实时同步）
+    QTimer *m_refreshTimer { nullptr };
+    
+    // 页面组件
+    QLabel *m_weatherLabel { nullptr };
+    QLabel *m_onlineDevicesLabel { nullptr };
+    QLabel *m_borrowedGearsLabel { nullptr };
+    QLabel *m_faultCountLabel { nullptr };
+    
+    // 雨具管理
+    QComboBox *m_gearStationCombo { nullptr };
+    QComboBox *m_gearSlotCombo { nullptr };
+    QTableWidget *m_gearTable { nullptr };
+    
+    // 用户管理
+    QLineEdit *m_userSearchInput { nullptr };
+    QTableWidget *m_userTable { nullptr };
+    
+    // 订单管理
+    QTableWidget *m_orderTable { nullptr };
+};
+
