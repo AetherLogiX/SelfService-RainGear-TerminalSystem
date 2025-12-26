86df665ae13615290f793dc0040f434dc9394157
Juvin <hdcqW@outlook.com>
2025-12-26 22:16:36 +0800

    StationDao.h功能初步定义

--- src/dao/StationDao.h
+++ src/dao/StationDao.h
@@ -3,4 +3,15 @@
 #include<QSqlDatabase>
 #include<QVector>
 #include<optional>
-#include<../Model/Station.hpp>
+#include<memory>
+
+#include<../Model/Stationlocal.hpp>
+#include"../Model/GlobalEnum.hpp"
+
+class StationDao{
+public:
+    //获取所有站点的完整信息
+    QVector<std::unique_ptr<Stationlocal>> selectAll(QSqlDatabase& db); 
+    //根据站点ID获取站点信息
+    std::unique_ptr<Stationlocal> selectById(QSqlDatabase& db, Station station);
+};
