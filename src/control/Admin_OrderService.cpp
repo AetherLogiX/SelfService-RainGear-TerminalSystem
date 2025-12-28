e6d4c60da54426a3fa7b7e639b237fb1c02fc01f
Juvin <hdcqW@outlook.com>
2025-12-29 03:10:53 +0800

    add订单的获取，最后一部分迁移...

---
+++ src/control/Admin_OrderService.cpp
@@ -0,0 +1,12 @@
+/*
+    管理员订单管理服务实现
+*/
+#include "Admin_OrderService.h"
+#include "../utils/ConnectionPool.h"
+
+//获取最近订单
+QVector<OrderInfo> Admin_OrderService::getRecentOrders(int limit) {
+    QSqlDatabase db = ConnectionPool::getThreadLocalConnection();
+    if (!db.isOpen()) return {};
+    return recordDao.selectRecent(db, limit);
+}
---
+++ src/control/Admin_OrderService.h
@@ -0,0 +1,18 @@
+/*
+    管理员订单管理服务
+*/
+#pragma once
+
+#include <QString>
+#include <QVector>
+#include "../dao/RecordDao.h"
+
+//复用DAO层的DTO
+using OrderInfo = OrderInfoDTO;
+
+class Admin_OrderService {
+public:
+    QVector<OrderInfo> getRecentOrders(int limit = 50); //获取最近订单
+private:
+    RecordDao recordDao;
+};
